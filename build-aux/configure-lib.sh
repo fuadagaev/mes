b53362cabb857b04b75a7a9a88758119a079d388
Jan Nieuwenhuizen <janneke@gnu.org>
2019-02-01 14:11:38 +0100

    mescc: Mes C Library: Support gcc-4.6.4: Add math stubs.
    * lib/math/ceil.c: New file.
    * lib/math/floor.c: New file.
    * lib/stub/atan2.c: New file.
    * lib/stub/cos.c: New file.
    * lib/stub/exp.c: New file.
    * lib/stub/log.c: New file.
    * lib/stub/modf.c: New file.
    * lib/stub/pow.c: New file.
    * lib/stub/sin.c: New file.
    * lib/stub/sqrt.c: New file.
    * lib/libc+gnu.c: Include them.
    * include/math.h (atan2, ceil, cos, exp, fabs, floor, log, modf, pow,
    sin, sqrt): Declare.
    * lib/math/fabs.c: Update.
    * lib/stub/ldexp.c: Update.

--- build-aux/configure-lib.sh
+++ build-aux/configure-lib.sh
@@ -228,7 +228,10 @@ lib/dirent/__getdirentries.c
 lib/dirent/closedir.c
 lib/dirent/opendir.c
 lib/dirent/readdir.c
+lib/math/ceil.c
 lib/math/fabs.c
+lib/math/floor.c
+lib/math/labs.c
 lib/mes/fdgets.c
 lib/posix/alarm.c
 lib/posix/execl.c
@@ -266,9 +269,12 @@ lib/string/strncat.c
 lib/string/strpbrk.c
 lib/string/strspn.c
 lib/stub/__cleanup.c
+lib/stub/atan2.c
 lib/stub/bsearch.c
 lib/stub/chown.c
+lib/stub/cos.c
 lib/stub/ctime.c
+lib/stub/exp.c
 lib/stub/fpurge.c
 lib/stub/freadahead.c
 lib/stub/frexp.c
@@ -280,9 +286,12 @@ lib/stub/getpgrp.c
 lib/stub/getpwnam.c
 lib/stub/getpwuid.c
 lib/stub/gmtime.c
+lib/stub/log.c
 lib/stub/mktime.c
+lib/stub/modf.c
 lib/stub/pclose.c
 lib/stub/popen.c
+lib/stub/pow.c
 lib/stub/rand.c
 lib/stub/setbuf.c
 lib/stub/setgrent.c
@@ -292,6 +301,8 @@ lib/stub/sigaddset.c
 lib/stub/sigblock.c
 lib/stub/sigdelset.c
 lib/stub/sigsetmask.c
+lib/stub/sin.c
+lib/stub/sqrt.c
 lib/stub/strftime.c
 lib/stub/sys_siglist.c
 lib/stub/system.c
