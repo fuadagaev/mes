3f5a2523ac8d9ff75eeabd0a3aa35f2e2f0a9294
Jan Nieuwenhuizen <janneke@gnu.org>
2019-02-08 13:56:25 +0100

    lib: Support gcc-4.6.4: Implement rewind.
    * lib/stdio/rewind.c: Move from ../stub.  Implement.
    * include/stdio.h (rewind): Declare.
    * build-aux/configure-lib.sh (libc_gnu_SOURCES): Update include.
    * lib/stub/rewind.c: Remove.

--- include/stdio.h
+++ include/stdio.h
@@ -77,6 +77,7 @@ int putc (int c, FILE * stream);
 int putchar (int c);
 int puts (char const *s);
 int remove (char const *file_name);
+void rewind (FILE * stream);
 int setvbuf (FILE * stream, char *buf, int mode, size_t size);
 int ungetc (int c, FILE * stream);
 long ftell (FILE * stream);
