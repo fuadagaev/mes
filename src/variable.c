a455aa8f5b319b2eae8c6b4ba74704e36bc1cf95
Jan Nieuwenhuizen <janneke@gnu.org>
2019-11-15 17:45:27 +0100

    core: Refactor lookup_variable.
    * src/variable.c (lookup_variable, lookup_variable_, lookup_ref): Drop
    lookup/env parameter.  Update users.
    * src/builtins.c (mes_builtins): Update registration.
    * include/mes/builtins.h: Update declarations.
    * include/mes/mes.h: Likewise.

--- src/variable.c
+++ src/variable.c
@@ -51,11 +51,9 @@ variable_bound_p (struct scm *var)
 }
 
 struct scm *
-lookup_variable (struct scm *lookup, struct scm *name, struct scm *define_p)
+lookup_variable (struct scm *name, struct scm *define_p)
 {
-  struct scm *handle = cell_f;
-  if (lookup->type = TPAIR)
-    handle = assq (name, lookup);
+  struct scm *handle = handle = assq (name, R0);
 
   if (handle == cell_f)
     {
@@ -68,15 +66,15 @@ lookup_variable (struct scm *lookup, struct scm *name, struct scm *define_p)
 }
 
 struct scm *
-lookup_variable_ (struct scm *lookup, char const* name)
+lookup_variable_ (char const* name)
 {
-  return lookup_variable (lookup, cstring_to_symbol (name), cell_f);
+  return lookup_variable (cstring_to_symbol (name), cell_f);
 }
 
 struct scm *
-lookup_ref (struct scm *lookup, struct scm *name)
+lookup_ref (struct scm *name)
 {
-  struct scm *x = lookup_variable (lookup, name, cell_f);
+  struct scm *x = lookup_variable (name, cell_f);
   if (x == cell_f)
     error (cell_symbol_unbound_variable, name);
   return x->cdr;
