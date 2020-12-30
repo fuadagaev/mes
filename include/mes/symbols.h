bd0378048c645738eb3d7edbcc4d6150f04572ab
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2020-12-30 11:12:00 +0100

    core: Make primitive-load return properly.
    * include/mes/symbols.h (cell_vm_primitive_load_return): New variable.
    (SYMBOL_MAX): Update.
    * src/eval-apply.c (eval_apply): Have primitive-load return properly.

--- include/mes/symbols.h
+++ include/mes/symbols.h
@@ -40,6 +40,7 @@ extern struct scm *cell_vm_begin_expand_eval;
 extern struct scm *cell_vm_begin_expand_macro;
 extern struct scm *cell_vm_begin_expand_primitive_load;
 extern struct scm *cell_vm_begin_primitive_load;
+extern struct scm *cell_vm_primitive_load_return;
 extern struct scm *cell_vm_begin_read_input_file;
 extern struct scm *cell_vm_call_with_current_continuation2;
 extern struct scm *cell_vm_call_with_values2;
@@ -136,8 +137,8 @@ extern struct scm *cell_type_broken_heart;
 extern struct scm *cell_symbol_program;
 extern struct scm *cell_symbol_test;
 
-// CONSTANT SYMBOL_MAX 113
-#define SYMBOL_MAX 113
+// CONSTANT SYMBOL_MAX 114
+#define SYMBOL_MAX 114
 
 // CONSTANT CELL_UNSPECIFIED 7
 #define CELL_UNSPECIFIED 7
