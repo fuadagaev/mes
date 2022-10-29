d270e5ef24419fa38cda8689915c48c2605c583b
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2022-10-29 13:17:58 +0200

    DRAFT lib: stat: Use SYS_stat64 for 32bit platforms.
    * include/linux/arm/syscall.h (SYS_stat64): New define.
    * include/linux/x86/syscall.h (SYS_stat64): Likewise.
    * lib/linux/stat.c (SYS_stat)[(__i386__ || __arm__) && SYS_stat64):
    Redefine to SYS_stat64.

--- lib/linux/fstat.c
+++ lib/linux/fstat.c
@@ -1,6 +1,6 @@
 /* -*-comment-start: "//";comment-end:""-*-
  * GNU Mes --- Maxwell Equations of Software
- * Copyright © 2018,2019 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+ * Copyright © 2018,2019,2022 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
  *
  * This file is part of GNU Mes.
  *
@@ -21,6 +21,11 @@
 #include <linux/syscall.h>
 #include <syscall.h>
 #include <sys/stat.h>
+
+#if (__i386__ || __arm__) && SYS_fstat64
+#undef SYS_fstat
+#define SYS_fstat SYS_fstat64
+#endif
 
 int
 fstat (int filedes, struct stat *statbuf)
