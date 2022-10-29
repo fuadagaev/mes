12b3a82d6ae2878ef6387023b4aa67c1e556616f
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2022-10-29 14:12:37 +0200

    squash! DRAFT lib: stdint: Remove __SIZEOF_LONG_LONG__ guard.

--- include/stdint.h
+++ include/stdint.h
@@ -55,9 +55,10 @@ typedef unsigned short uint16_t;
 typedef short int16_t;
 typedef unsigned uint32_t;
 typedef int int32_t;
+#if HAVE_LONG_LONG
 typedef unsigned long long uint64_t;
 typedef long long int64_t;
-
+#endif
 typedef int intmax_t;
 typedef unsigned uintmax_t;
 
